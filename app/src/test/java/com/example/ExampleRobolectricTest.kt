74717bd5e9c11496bd1f3119f7bebc868d4d47a5
ffcct2755-prog <ffcct2755@gmail.com>
2026-07-09 23:02:46 +0600

    test: update app name expectation in Robolectric test
    Update the test case to reflect the rebranded application name "Alif Go VPN".

--- app/src/test/java/com/example/ExampleRobolectricTest.kt
+++ app/src/test/java/com/example/ExampleRobolectricTest.kt
@@ -16,6 +16,6 @@ class ExampleRobolectricTest {
   fun `read string from context`() {
     val context = ApplicationProvider.getApplicationContext<Context>()
     val appName = context.getString(R.string.app_name)
-    assertEquals("Alif VPN", appName)
+    assertEquals("Alif Go VPN", appName)
   }
 }
